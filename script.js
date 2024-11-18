0f3af161f3f10bd6156d100fc523e4125550b408
Necromancy501 <94140246+Necromancy501@users.noreply.github.com>
2024-11-18 15:23:07 +0800

    Add Cursor and Input behavior

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+
+// Main Listener Function
+// PENDING: REFACTOR THIS SO LABEL GETS PASSED INSTEAD OF OBJECT
+function inputHandler(label){
+    let sliceIndex = stringDisplay.length + cursorCounter;
+    symbols = ['C','DEL','<=','=>','/%','**','+-','=','DOT']
+
+    if (symbols.includes(label)){
+        switch(label) {
+            case 'C': 
+                stringDisplay = '';
+                break;
+            case 'DEL':
+                stringDisplay = stringDisplay.slice(0,sliceIndex);
+                break;
+            case '<=':
+                if (cursorCounter === stringDisplay.length * -1){
+                    console.log('WRAP PENDING');
+                }
+                else {
+                    cursorCounter--;
+                }
+                break;
+            case '=>':
+                if (cursorCounter === 0){
+                    console.log('WRAP PENDING');
+                }
+                else {
+                    cursorCounter++;
+                }
+                break;
+        }
+    }
+    else{
+        let stringArray = stringDisplay.split('');
+        stringArray.splice(sliceIndex, 0, label);
+        stringDisplay = stringArray.join(''); 
+    }
+    displayOnPage.textContent = stringDisplay;
+}
+
+
+
 // Constants for sketch layout.
 const BUTTONS_WIDTH = 4;
 const BUTTONS_HEIGHT = 4;
@@ -14,7 +57,7 @@ buttonsContainer.style.justifyContent = 'center';
 
 // Stickers for buttons
 let stickersCounter = 0;
-const stickers = ['C','DEL','←','→','7','8','9','/%','4','5','6','**','1', '2', '3', '+-', "DOT", "0", "="];
+const stickers = ['C','DEL','<=','=>','7','8','9','/%','4','5','6','**','1', '2', '3', '+-', "DOT", "0", "="];
 
 // Creating buttons inside buttons container
 for (i=0; i<BUTTONS_HEIGHT; i++){
@@ -32,7 +75,7 @@ for (i=0; i<BUTTONS_HEIGHT; i++){
         // Creation of the buttons
         const button = document.createElement("button");
         button.dataset.position = `${n},${i}`;
-        button.dataset.label = `${stickers[stickersCounter]}`
+        button.dataset.label = `${stickers[stickersCounter]}`;
         button.textContent = stickers[stickersCounter++];
 
         // Styling of the buttons
@@ -43,12 +86,6 @@ for (i=0; i<BUTTONS_HEIGHT; i++){
         button.style.fontSize = "27px";
         button.style.flex = "1, 1, auto";
         button.style.border = "8px solid #374b43";
-
-        // Arrow specifics
-        if (button.textContent == "→" || button.textContent == "←"){
-            button.style.fontSize = "40px";
-            button.style.fontWeight = "bold";
-        }
 
         buttonsRow.appendChild(button);
     }
@@ -71,6 +108,7 @@ for (a=0;a<BUTTONS_BOTTOM;a++){
     // Creation of the buttons
     const button = document.createElement("button");
     button.dataset.position = `${4},${a}`;
+    button.dataset.label = `${stickers[stickersCounter]}`;
     button.textContent = stickers[stickersCounter++];
 
     // Styling of the buttons
@@ -90,7 +128,19 @@ for (a=0;a<BUTTONS_BOTTOM;a++){
     buttonsLastRow.appendChild(button);
 }
 
+// Buttons get loaded into the DOM
 buttonsContainer.appendChild(buttonsLastRow);
 
+// Input behavior for buttons
 
+const buttonsOnPage = document.querySelector(".buttons-container");
+const displayOnPage = document.querySelector(".display-top");
 
+let stringDisplay = '';
+let cursorCounter = 0;
+
+buttonsOnPage.addEventListener("click", (e) => {
+    if (e.target.dataset.label){
+        inputHandler(e.target.dataset.label);
+    }
+});
